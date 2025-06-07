9ca701eb7a185473fe0a75891b93a9f7d02ebff6
imprint-sh <104689022+imprint-sh@users.noreply.github.com>
2025-06-07 03:50:37 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ADD THIS LINE: Use your repository name
+  base: '/sanctum-os-vite/', 
   plugins: [react()],
 })
